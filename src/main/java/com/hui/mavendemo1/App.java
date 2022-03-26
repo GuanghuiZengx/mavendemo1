a3734a530e8d4bd46710092b0fc35689eff5f17b
GuanghuiZengx <1332514665@qq.com>
2022-03-26 17:24:48 +0800

    test

---
+++ src/main/java/com/hui/mavendemo1/App.java
@@ -0,0 +1,13 @@
+package com.hui.mavendemo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {//123
+        System.out.println( "Hello World!" );
+    }
+}
